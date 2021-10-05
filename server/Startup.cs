b10ad575cdc089fa9a8d00ccd3504d8670b612c0
Pedro Delavechia Risso <pedro.delavechia@soitic.com>
2021-10-05 12:28:03 -0300

    Changes the API route prefix to string.Empty
    Makes the Swagger page to load without having to write .../swagger/index.html at the end of the URL

--- server/Startup.cs
+++ server/Startup.cs
@@ -41,7 +41,10 @@ namespace server
             {
                 app.UseDeveloperExceptionPage();
                 app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "server v1"));
+                app.UseSwaggerUI(c => {
+                    c.SwaggerEndpoint("/swagger/v1/swagger.json", "server v1");
+                    c.RoutePrefix = string.Empty;
+                });
             }
 
             app.UseHttpsRedirection();
