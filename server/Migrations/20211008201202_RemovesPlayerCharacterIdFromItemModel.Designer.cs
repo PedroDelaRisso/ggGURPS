308e68f4ffcd35a8f0d850363d9ea1ba6a1e7bf0
Pedro Delavechia Risso <pedro.delavechia@soitic.com>
2021-10-08 17:13:17 -0300

    Migrations

--- server/Migrations/20211008201202_RemovesPlayerCharacterIdFromItemModel.Designer.cs
+++ server/Migrations/20211008201202_RemovesPlayerCharacterIdFromItemModel.Designer.cs
@@ -2,15 +2,17 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using server.Models;
 
 namespace server.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20211008201202_RemovesPlayerCharacterIdFromItemModel")]
+    partial class RemovesPlayerCharacterIdFromItemModel
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
