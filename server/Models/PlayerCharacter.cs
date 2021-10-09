b6ff410208f7f57d4a9777221e187c9e5b614bc2
Pedro Delavechia Risso <p.delavechia.r@gmail.com>
2021-10-08 21:27:28 -0300

    Reduced complexity even further

---
+++ server/Models/PlayerCharacter.cs
@@ -0,0 +1,34 @@
+using System.Collections.Generic;
+
+public class PlayerCharacter
+{
+    public long Id { get; set; }
+
+    public string CharacterName { get; set; }
+    public string Age { get; set; }
+    public string Gender { get; set; }
+    public string PlayerName { get; set; }
+
+    public int Points { get; set; }
+
+    public int Strength { get; set; }
+    public int Dexterity { get; set; }
+    public int Inteligence { get; set; }
+    public int Health { get; set; }
+
+    public int FatiguePoints { get; set; }
+    public int HitPoints { get; set; }
+
+    public int Will { get; set; }
+    public int Perception { get; set; }
+
+    public long GameMasterId { get; set; }
+    public GameMaster GameMaster { get; set; }
+
+    public PlayerCharacter(string characterName, string age, string gender)
+    {
+        CharacterName = characterName;
+        Age = age;
+        Gender = gender;
+    }
+}
