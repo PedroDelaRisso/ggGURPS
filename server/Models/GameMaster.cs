bfb4e62cae1c272813c718820e9065a2d4087440
Pedro Delavechia Risso <p.delavechia.r@gmail.com>
2021-10-08 21:33:50 -0300

    Update GameMaster.cs

--- server/Models/GameMaster.cs
+++ server/Models/GameMaster.cs
@@ -1,18 +1,15 @@
 using System.Collections.Generic;
 
-namespace server.Models
+public class GameMaster
 {
-    public class GameMaster
+    public long Id { get; set; }
+
+    public string Name { get; set; }
+
+    public ICollection<PlayerCharacter> PlayerCharacters { get; set; }
+
+    public GameMaster(string name)
     {
-        public long Id { get; set; }
-
-        public string Name { get; set; }
-
-        public ICollection<PlayerCharacter> PlayerCharacters { get; set; }
-
-        public GameMaster(string name)
-        {
-            Name = name;
-        }
+        Name = name;
     }
 }
