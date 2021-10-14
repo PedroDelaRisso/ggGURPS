682dec13ff2961a841baa6206d747a48a8d05945
Pedro Delavechia Risso <pedro.delavechia@soitic.com>
2021-10-14 12:18:20 -0300

    Improvement in the Item model and controller

--- server/Controllers/ItemController.cs
+++ server/Controllers/ItemController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Threading.Tasks;
 using System.Linq;
 using Microsoft.AspNetCore.Mvc;
+using System;
 
 [ApiController]
 [Route("[controller]")]
@@ -18,76 +19,90 @@ public class ItemController : ControllerBase
     [HttpGet]
     public async Task<ActionResult<List<Item>>> Get()
     {
-        var items = await _context.Items.ToListAsync();
-        return Ok(items);
+        try
+        {
+            var items = await _context.Items.ToListAsync();
+            if (!items.Any())
+                return NotFound("There are no saved Items.");
+            return items;
+        } catch (Exception ex)
+        {
+            return Conflict(ex.Message);
+        }
     }
 
     [HttpPost]
     public async Task<ActionResult<Item>> Post([FromBody] Item item)
     {
-        Item newItem = new Item();
-        // Generic Item
-        if (item.ItemType == ItemType.Item)
+        try
         {
-            newItem = new Item(
+            Item newItem = new Item(
                 item.Id,
                 item.Name,
                 item.Description,
                 item.ItemType,
                 item.Price,
-                item.Weight
-            );
-        }
-
-        // Armor
-        else if (item.ItemType == ItemType.Armor)
-        {
-            newItem = new Item(
-                item.Id,
-                item.Name,
-                item.Description,
-                item.ItemType,
+                item.Weight,
                 item.DamageReduction,
-                item.Price,
-                item.Weight
-            );
-        }
-
-        // Weapon
-        else if (item.ItemType == ItemType.Weapon)
-        {
-            newItem = new Item(
-                item.Id,
-                item.Name,
-                item.Description,
-                item.ItemType,
                 item.SwingDamageDice,
                 item.SwingDamageModifier,
                 item.ThrustDamageDice,
                 item.ThrustDamageModifier,
-                item.Price,
-                item.Weight
-            );
-        }
-
-        // Gun
-        else if (item.ItemType == ItemType.Gun)
-        {
-            newItem = new Item(
-                item.Id,
-                item.Name,
-                item.Description,
-                item.ItemType,
                 item.Recoil,
+                item.Range,
                 item.RateOfFire,
                 item.DamageDice,
-                item.DamageModifier,
-                item.Price,
-                item.Weight
+                item.DamageModifier
             );
+            if (newItem.ItemType == ItemType.Item)
+            {
+                newItem.DamageReduction = -999;
+                newItem.SwingDamageDice = -999;
+                newItem.SwingDamageModifier = -999;
+                newItem.ThrustDamageDice = -999;
+                newItem.ThrustDamageModifier = -999;
+                newItem.Recoil = -999;
+                newItem.Range = -999;
+                newItem.RateOfFire = -999;
+                newItem.DamageDice = -999;
+                newItem.DamageModifier = -999;
+            } 
+            else if (newItem.ItemType == ItemType.Armor)
+            {
+                newItem.SwingDamageDice = -999;
+                newItem.SwingDamageModifier = -999;
+                newItem.ThrustDamageDice = -999;
+                newItem.ThrustDamageModifier = -999;
+                newItem.Recoil = -999;
+                newItem.Range = -999;
+                newItem.RateOfFire = -999;
+                newItem.DamageDice = -999;
+                newItem.DamageModifier = -999;
+            }
+            else if (newItem.ItemType == ItemType.Gun)
+            {
+                newItem.DamageReduction = -999;
+                newItem.SwingDamageDice = -999;
+                newItem.SwingDamageModifier = -999;
+                newItem.ThrustDamageDice = -999;
+                newItem.ThrustDamageModifier = -999;
+            }
+            else if (newItem.ItemType == ItemType.Weapon)
+            {
+                newItem.DamageReduction = -999;
+                newItem.Recoil = -999;
+                newItem.Range = -999;
+                newItem.RateOfFire = -999;
+                newItem.DamageDice = -999;
+                newItem.DamageModifier = -999;
+            }
+            
+            _context.Items.Add(newItem);
+            await _context.SaveChangesAsync();
+            return newItem;
+        } catch (Exception ex)
+        {
+            return Conflict(ex.Message);
         }
-        _context.Items.Add(newItem);
-        await _context.SaveChangesAsync();
-        return Ok(newItem);
     }
 }
--- server/Models/Item.cs
+++ server/Models/Item.cs
@@ -5,108 +5,58 @@ public class Item
     public string Name { get; set; }
     public string Description { get; set; }
     public ItemType ItemType { get; set; }
-    public double? Price{ get; set; }
-    public double? Weight{ get; set; }
+    public double Price{ get; set; }
+    public double Weight{ get; set; }
 
     // Armor
-    public uint DamageReduction { get; set; }
+    public int DamageReduction { get; set; }
 
     // Weapon
-    public uint SwingDamageDice { get; set; }
+    public int SwingDamageDice { get; set; }
     public int SwingDamageModifier { get; set; }
-    public uint ThrustDamageDice { get; set; }
+    public int ThrustDamageDice { get; set; }
     public int ThrustDamageModifier { get; set; }
 
     // Gun
-    public uint Recoil { get; set; }
-    public uint Range { get; set; }
-    public uint RateOfFire { get; set; }
-    public uint DamageDice { get; set; }
+    public int Recoil { get; set; }
+    public int Range { get; set; }
+    public int RateOfFire { get; set; }
+    public int DamageDice { get; set; }
     public int DamageModifier { get; set; }
 
-    // Empty Item Constructor
-    public Item() { }
-
-    // Item Item constructor
-    public Item(long id, string name, string description, ItemType itemType, double? price, double? weight)
+    public Item(long id,
+                string name,
+                string description,
+                ItemType itemType,
+                double price,
+                double weight,
+                int damageReduction,
+                int swingDamageDice,
+                int swingDamageModifier,
+                int thrustDamageDice,
+                int thrustDamageModifier,
+                int recoil,
+                int range,
+                int rateOfFire,
+                int damageDice,
+                int damageModifier
+    )
     {
         Id = id;
         Name = name;
         Description = description;
         ItemType = itemType;
-        if (price != null)
-        {
-            Price = price;
-        }
-        if (weight != null)
-        {
-            Weight = weight;
-        }
-    }
-
-    // Armor Item constructor
-    public Item(long id, string name, string description, ItemType itemType, uint damageReduction, double? price, double? weight)
-    {
-        Id = id;
-        Name = name;
-        Description = description;
-        ItemType = itemType;
-
+        Price = price;
+        Weight = weight;
         DamageReduction = damageReduction;
-
-        if (price != null)
-        {
-            Price = price;
-        }
-        if (weight != null)
-        {
-            Weight = weight;
-        }
-    }
-
-    // Weapon Item constructor
-    public Item(long id, string name, string description, ItemType itemType, uint swingDamageDice, int swingDamageModifier, uint thrustDamageDice, int thrustDamageModifier, double? price, double? weight)
-    {
-        Id = id;
-        Name = name;
-        Description = description;
-        ItemType = itemType;
-
         SwingDamageDice = swingDamageDice;
         SwingDamageModifier = swingDamageModifier;
         ThrustDamageDice = thrustDamageDice;
         ThrustDamageModifier = thrustDamageModifier;
-
-        if (price != null)
-        {
-            Price = price;
-        }
-        if (weight != null)
-        {
-            Weight = weight;
-        }
-    }
-
-    // Gun Item constructor
-    public Item(long id, string name, string description, ItemType itemType, uint recoil, uint rateOfFire, uint damageDice, int damageModifier, double? price, double? weight)
-    {
-        Id = id;
-        Name = name;
-        Description = description;
-        ItemType = itemType;
-
         Recoil = recoil;
+        Range = range;
         RateOfFire = rateOfFire;
         DamageDice = damageDice;
         DamageModifier = damageModifier;
-
-        if (price != null)
-        {
-            Price = price;
-        }
-        if (weight != null)
-        {
-            Weight = weight;
-        }
     }
 }
