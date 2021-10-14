652be702ba2144f0e5dff0bf7be06487a1c43e89
Pedro Delavechia Risso <p.delavechia.r@gmail.com>
2021-10-13 21:03:40 -0300

    Creates Items and Inventories

--- server/ApplicationDbContext.cs
+++ server/ApplicationDbContext.cs
@@ -4,6 +4,9 @@ public class ApplicationDbContext : DbContext
 {
     public DbSet<PlayerCharacter> PlayerCharacters { get; set; }
     public DbSet<GameMaster> GameMasters { get; set; }
+    public DbSet<Inventory> Inventories { get; set; }
+    public DbSet<Item> Items { get; set; }
+
 
     public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) {}
 }
