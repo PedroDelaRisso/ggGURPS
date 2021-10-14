15e13103ed4256d50b797f59dbd497b285c8e20a
Pedro Delavechia Risso <p.delavechia.r@gmail.com>
2021-10-13 21:03:51 -0300

    Items and Inventories migration

--- server/Migrations/20211014000318_CreatesItemsAndInventories.Designer.cs
+++ server/Migrations/20211014000318_CreatesItemsAndInventories.Designer.cs
@@ -3,14 +3,16 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace server.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20211014000318_CreatesItemsAndInventories")]
+    partial class CreatesItemsAndInventories
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ server/Migrations/20211014000318_CreatesItemsAndInventories.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace server.Migrations
+{
+    public partial class CreatesItemsAndInventories : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Inventories",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CharacterId = table.Column<long>(type: "bigint", nullable: false),
+                    ItemId = table.Column<long>(type: "bigint", nullable: false),
+                    Amount = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Inventories", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Items",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    ItemType = table.Column<int>(type: "int", nullable: false),
+                    Price = table.Column<double>(type: "float", nullable: true),
+                    Weight = table.Column<double>(type: "float", nullable: true),
+                    DamageReduction = table.Column<long>(type: "bigint", nullable: true),
+                    SwingDamageDice = table.Column<long>(type: "bigint", nullable: true),
+                    SwingDamageModifier = table.Column<int>(type: "int", nullable: true),
+                    ThrustDamageDice = table.Column<long>(type: "bigint", nullable: true),
+                    ThrustDamageModifier = table.Column<int>(type: "int", nullable: true),
+                    Recoil = table.Column<long>(type: "bigint", nullable: true),
+                    Range = table.Column<long>(type: "bigint", nullable: true),
+                    RateOfFire = table.Column<long>(type: "bigint", nullable: true),
+                    DamageDice = table.Column<long>(type: "bigint", nullable: true),
+                    DamageModifier = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Items", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Inventories");
+
+            migrationBuilder.DropTable(
+                name: "Items");
+        }
+    }
+}
