54f251d82ea9c80d6345b653f6f00b2b9e5ab3ea
Pedro Delavechia Risso <37818660+PedroDelaRisso@users.noreply.github.com>
2021-10-13 21:11:27 -0300

    Creates Items and Inventories (#10)
    * Creates Items and Inventories
    
    * Items and Inventories migration

--- server/ApplicationDbContext.cs
+++ server/ApplicationDbContext.cs
@@ -4,6 +4,9 @@ public class ApplicationDbContext : DbContext
 {
     public DbSet<Character> Characters { get; set; }
     public DbSet<GameMaster> GameMasters { get; set; }
+    public DbSet<Inventory> Inventories { get; set; }
+    public DbSet<Item> Items { get; set; }
+
 
     public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) {}
 }
