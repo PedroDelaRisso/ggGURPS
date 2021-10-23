625edd74ea123d5e32758cc84ef0c312a09a7089
Pedro Delavechia Risso <pedro.delavechia@soitic.com>
2021-10-23 11:16:20 -0300

    Deleted ghost files from the last refactoring

--- server/src/ggGURPS.WebApi/Models/Players/GetPlayersByCampaignIdDTO.cs
+++
@@ -1,8 +0,0 @@
-namespace ggGURPS.WebApi.Models.Players
-{
-    public class GetPlayersByCampaignIdDTO
-    {
-        public int Id { get; set; }
-        public string Name { get; set; }
-    }
-}
