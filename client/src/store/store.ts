2fbf6dbd611dd52c937d200ddd3f9eac67f1a668
PedroDelaRisso <p.delavechia.r@gmail.com>
2022-02-20 21:52:31 -0300

    Removed old Svelte template

--- client/src/store/store.ts
+++
@@ -1,7 +0,0 @@
-import { writable, derived } from "svelte/store";
-
-export const apiData = writable([]);
-
-export const gameMasters = derived(apiData, ($apiData) => {
-    return $apiData.map((gm) => gm.name);
-})
