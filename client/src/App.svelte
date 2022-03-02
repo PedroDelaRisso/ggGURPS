f862e1d88940f50ff6b08bd09ac2ca49925d9970
PedroDelaRisso <p.delavechia.r@gmail.com>
2022-03-02 11:39:42 -0300

    Calls new components on App.svelte

--- client/src/App.svelte
+++ client/src/App.svelte
@@ -1,7 +1,11 @@
 <script lang="ts">
+import CampaignList from "./components/Campaign/CampaignList.svelte";
 import GameMasterList from "./components/GameMasters/GameMasterList.svelte";
+import PlayerList from "./components/Players/PlayerList.svelte";
 </script>
 
 <main>
   <GameMasterList />
+  <PlayerList />
+  <CampaignList />
 </main>
